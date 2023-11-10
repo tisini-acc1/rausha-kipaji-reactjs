f6a1d9ff91c5006ab6eb2d13e4d2f4be20f8eebb
JI-Maina <irungujmaina@gmail.com>
2023-11-10 18:49:16 +0300

    initial commit

---
+++ components/home/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="h-screen w-full">
+      <div className="">Hero</div>
+    </section>
+  );
+};
+
+export default Hero;
