1f95f866a45eae314679360a87d2491845e32ad1
JI-Maina <irungujmaina@gmail.com>
2023-12-29 12:06:33 +0300

    Created gallery page

---
+++ src/components/gallery/constants.tsx
@@ -0,0 +1,58 @@
+export const seasons = [
+  {
+    year: "2023",
+    theme: "Going green through sports",
+    image: "/images/23/rausha-1.jpg",
+  },
+  {
+    year: "2022",
+    theme: "Going green through sports",
+    image: "/images/23/rausha-2.jpg",
+  },
+  {
+    year: "2021",
+    theme: "Going green through sports",
+    image: "/images/23/rausha-3.jpg",
+  },
+  {
+    year: "2018",
+    theme: "Going green through sports",
+    image: "/images/23/rausha-4.jpg",
+  },
+];
+
+export const images18 = [
+  "/images/23/rausha-1.jpg",
+  "/images/23/rausha-2.jpg",
+  "/images/23/rausha-3.jpg",
+  "/images/23/rausha-4.jpg",
+  "/images/23/rausha-5.jpg",
+  "/images/23/rausha-6.jpg",
+];
+
+export const images21 = [
+  "/images/23/rausha-1.jpg",
+  "/images/23/rausha-2.jpg",
+  "/images/23/rausha-3.jpg",
+  "/images/23/rausha-4.jpg",
+  "/images/23/rausha-5.jpg",
+  "/images/23/rausha-6.jpg",
+];
+
+export const images22 = [
+  "/images/23/rausha-1.jpg",
+  "/images/23/rausha-2.jpg",
+  "/images/23/rausha-3.jpg",
+  "/images/23/rausha-4.jpg",
+  "/images/23/rausha-5.jpg",
+  "/images/23/rausha-6.jpg",
+];
+
+export const images23 = [
+  "/images/23/rausha-1.jpg",
+  "/images/23/rausha-2.jpg",
+  "/images/23/rausha-3.jpg",
+  "/images/23/rausha-4.jpg",
+  "/images/23/rausha-5.jpg",
+  "/images/23/rausha-6.jpg",
+];
--- src/pages/gallery/GalleryPage.tsx
+++ src/pages/gallery/GalleryPage.tsx
@@ -1,5 +1,32 @@
+import { seasons } from "@components/gallery/constants";
+
 const GalleryPage = () => {
-  return <div>GalleryPage</div>;
+  return (
+    <main className="w-full">
+      <section className="bg-hero bg-no-repeat bg-cover bg-center min-h-screen text-white relative pt-[110px] lg:pt-[200px] flex justify-center">
+        <div className="bg-red-900 bg-opacity-70 absolute inset-0"></div>
+
+        <div className="grid gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-4 absolute">
+          {seasons.map((season) => (
+            <div
+              key={season.year}
+              className="w-full max-w-[290px] rounded-md bg-white hover:bg-red-500 hover:text-white  transition cursor-pointer"
+            >
+              <div className="">
+                <img src={season.image} alt="" className="rounded-t-md" />
+              </div>
+              <div className="text-black p-2 space-y-2">
+                <p className="font-semibold lg:text-xl">
+                  {season.year} gallery
+                </p>
+                <h1 className="text-sm">{season.theme}</h1>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default GalleryPage;
