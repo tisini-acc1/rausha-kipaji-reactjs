b7041cf7d4e29b1cd8bf9f559262c2eef57c29cc
McMaina <irutujmaina@gmail.com>
2024-04-15 13:39:28 +0300

    modified scores header to show active date

--- src/components/fixtures/FixturesHeader.tsx
+++ src/components/fixtures/FixturesHeader.tsx
@@ -1,36 +1,35 @@
 type HeaderProps = {
+  selectedDate: string;
   dates: string[];
   setDates: (d: string) => void;
 };
 
-export const FixturesHeader = ({ dates, setDates }: HeaderProps) => {
+export const FixturesHeader = ({
+  selectedDate,
+  dates,
+  setDates,
+}: HeaderProps) => {
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "short" });
+    return `${day} ${dayOfWeek}`;
+  };
+
   return (
     <div className="bg-tisini bg-center h-32 sticky top-0">
       <nav className="flex md:gap-4 gap-2 absolute bottom-1">
-        <button
-          className="px-4 border rounded-md bg-orange hover:bg-green-600 font-semibold whitespace-nowrap"
-          onClick={() => setDates(dates[0])}
-        >
-          18 Thu
-        </button>
-        <button
-          className="px-4 border rounded-md bg-orange hover:bg-green-600 font-semibold whitespace-nowrap"
-          onClick={() => setDates(dates[1])}
-        >
-          19 Fri
-        </button>
-        <button
-          className="px-4 border rounded-md bg-orange hover:bg-green-600 font-semibold whitespace-nowrap"
-          onClick={() => setDates(dates[2])}
-        >
-          20 Sat
-        </button>
-        <button
-          className="px-4 border rounded-md bg-orange hover:bg-green-600 font-semibold whitespace-nowrap"
-          onClick={() => setDates(dates[3])}
-        >
-          21 Sun
-        </button>
+        {dates.map((date, index) => (
+          <button
+            key={index}
+            className={`px-4 border rounded-md hover:bg-green-600 font-semibold whitespace-nowrap ${
+              selectedDate === date ? "bg-green-600" : "bg-orange"
+            }`}
+            onClick={() => setDates(date)}
+          >
+            {formatDate(date)}
+          </button>
+        ))}
       </nav>
     </div>
   );
